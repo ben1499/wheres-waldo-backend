99916c9847bc1ac6926c57f2288d09a64c77eae0
Reuben <reubenthomas99@gmail.com>
2024-06-24 07:39:49 +0530

    Initial setup

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: "Where's Waldo Backend" });
+});
+
+module.exports = router;
