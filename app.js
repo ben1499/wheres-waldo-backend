5d0bcf2a93897a60effa88978d740ddfdcc0eced
Reuben <reubenthomas99@gmail.com>
2024-07-10 19:45:52 +0530

    Add security and performance improvements

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const logger = require('morgan');
 require('dotenv').config()
 const mongoose = require("mongoose");
 const cors = require("cors");
+const compression = require("compression");
+const helmet = require("helmet");
+const RateLimit = require("express-rate-limit");
 
 const indexRouter = require('./routes/index');
 
@@ -16,6 +19,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(cors());
+
+// Set up rate limiter: maximum of twenty requests per minute
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 30,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+// Compress all routes
+app.use(compression()); 
+
+// Add helmet to the middleware chain.
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'"],
+    },
+  }),
+);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,8 +50,6 @@ main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect(process.env.MONGODB_URI);
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 app.use('/', indexRouter);
